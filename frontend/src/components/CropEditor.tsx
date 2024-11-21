291b76cec320a5394613606fb29cd7a75ee38d66
VdanielVPL <dkulak21@gmail.com>
2024-11-21 15:02:20 +0100

    Added more styles + prep for 'Image Cropping' feature

---
+++ frontend/src/components/CropEditor.tsx
@@ -0,0 +1,5 @@
+export default function CropEditor() {
+    return (
+        <div className="cropEditor"></div>
+    );
+}
--- frontend/src/components/CropImageContainer.tsx
+++ frontend/src/components/CropImageContainer.tsx
@@ -1,9 +1,10 @@
-import { useEffect, useRef, CSSProperties, useContext } from "react";
+import { useEffect, useRef, CSSProperties, useContext, useState, ReactEventHandler, SyntheticEvent } from "react";
 import { OnFileDrop, OnFileDropOff } from "../../wailsjs/runtime/runtime";
 import { HandleDrop, OpenImage } from "../../wailsjs/go/main/App";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCloudArrowUp } from "@fortawesome/free-solid-svg-icons";
 import { ImageContext } from "./contexts/ImageContext";
+import CropEditor from "./CropEditor";
 
 export function InputImageContainer() {
     // const [filePath, setFilePath] = useState<string>('');
@@ -67,12 +68,29 @@ export function InputImageContainer() {
         }
     }
 
+    function onImageLoad(e: SyntheticEvent<HTMLImageElement>) {
+        if(imageContainer.current != null){
+            console.log(e.currentTarget.width, e.currentTarget.height);
+            if (e.currentTarget.width < e.currentTarget.height) {
+                imageContainer.current.style.width = "fit-content";
+                imageContainer.current.style.height = "100%";
+                e.currentTarget.style.height = "100%";
+            } else {
+                imageContainer.current.style.height = "fit-content";
+                imageContainer.current.style.width = "100%";
+                e.currentTarget.style.width = "100%";
+            }
+            imageContainer.current.style.borderRadius = '0px';
+        };
+    }
+
     return (
-        <div ref={imageContainer} className='imageContainer' onDrop={handleURLDrop} style={{'--wails-drop-target': 'drop'} as CSSProperties} onClick={openDialog}>
-            <img src={cropImage} style={{userSelect: 'none', pointerEvents: 'none'}} draggable={false}></img>
+        <div ref={imageContainer} className='imageContainer imageContainerCrop' onDrop={handleURLDrop} style={{'--wails-drop-target': 'drop', backgroundColor: cropImage!="" && "rgba(255,255,255,0)"} as CSSProperties} onClick={openDialog}>
+            <img src={cropImage} style={{userSelect: 'none', pointerEvents: 'none'}} draggable={false} onLoad={onImageLoad}></img>
             <div style={{position: 'absolute', height: '100%', width: '100%', color: 'black', display: 'flex', justifyContent: 'center', alignItems: 'center', zIndex: -1,}}>
-                <FontAwesomeIcon icon={faCloudArrowUp} color="lightgray" style={{width: '60%', height: '60%', fontSize: '60%'}} />
+                <FontAwesomeIcon icon={faCloudArrowUp} color="lightgray" style={{width: '60%', height: '60%', fontSize: '60%', maxHeight: '200px', maxWidth: '200px'}} />
             </div>
+            {cropImage && <CropEditor />}
         </div>
     )
 }
